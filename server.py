af1ce9872ee8f449e49308addd55e9c457d984c1
alfa <alexisalfaro505@gmai.com>
2025-09-04 17:30:45 -0400

    Create the server file

---
+++ server.py
@@ -0,0 +1,37 @@
+''' Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask('Emotion Detector')
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    ''' This code receives the text from the HTML interface and 
+        runs sentiment analysis over it using sentiment_analysis()
+        function. The output returned shows the label and its confidence 
+        score for the provided text.
+    '''
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    
+    output = 'For the given statement, the system response is '
+    for key, value in response.items():
+        if isinstance(value, float):
+            output += f'{key} : {value} '
+        else: 
+            output += f'.The dominant emotion is {value}'
+
+    return output
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', port = 5000)
