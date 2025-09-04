54409001009bf7af5b9240471e29895b6b22053f
alfa <alexisalfaro505@gmai.com>
2025-09-04 16:48:31 -0400

    Change the code for the output to be formatted

--- emotion_detection.py
+++ emotion_detection.py
@@ -9,5 +9,18 @@ def emotion_detector(text_to_analyze):
     myobj = {'raw_document': {'text': text_to_analyze} }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers = header)
+    formatted_response = json.loads(response.text)
+    result = formatted_response["emotionPredictions"][0]["emotion"]
+    result['dominant_emotion'] = ''
+    max_value = 0
+    for key, value in result.items():
+        try:
+            if value > max_value:
+                result['dominant_emotion'] = key
+                max_value = value
+        except TypeError:
+            pass
 
-    return response.text
+    return result
+
+emotion_detector('I love learning!')
